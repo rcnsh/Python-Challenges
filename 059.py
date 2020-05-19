0a970dad37e155ad76ef8728e804e87b70d6fbfc
Jake <jacobjameswiltshire@gmail.com>
2020-05-19 09:41:27 +0100

    solved bug in 59

--- 059.py
+++ 059.py
@@ -3,9 +3,9 @@ colouranswer=random.choice( ["GRAY", "RED", "BLUE","GREEN","YELLOW"] )
 
 colour=random.choice( ["You must be feeling GRAY right now.", "Are you RED with anger?", "Don't feel BLUE, be happy!","You must be feeling GREEN with envy. ","You must be mellow, or YELLOW"] )
 
-input1=input("Enter a colour: GRAY, RED, BLUE, GREEN, YELLOW"
+input1=input("Enter a colour: GRAY, RED, BLUE, GREEN, YELLOW")
 
-if (colouranswer) = (input1):
+if colouranswer == input1:
     print ("WELL DONE!!!")
 else:
     print (colour)
