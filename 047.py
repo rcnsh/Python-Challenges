7f523f104f43915f89414c0667ccbd3a604d2ebe
Jacob Wiltshire <49075095+RCNOverwatcher@users.noreply.github.com>
2020-05-20 12:04:15 +0100

    Update 047.py
    Added equality operator (thanks deepsource! :)

--- 047.py
+++ 047.py
@@ -9,5 +9,5 @@ num1 = int(input("Pick a number"))
 num2 = int(input("Pick another number"))
 
 again = "y"
-while again = "y":
+while again == "y":
     num  = int(input("Do you want to add another number?"))
