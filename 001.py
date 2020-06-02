55ab43f18898c3a9f626ea8df6f2044f0a78a7ed
Jake <jacobjameswiltshire@gmail.com>
2020-06-02 09:43:32 +0100

    added some descriptions

--- 001.py
+++ 001.py
@@ -1,2 +1,4 @@
+#Ask for their first name and display the output message “Hello [First Name]”
+
 var = input("What is your name?\n")
 print("Hello", var)
--- 002.py
+++ 002.py
@@ -1,3 +1,6 @@
+#Ask for their first name and then ask for their surname and display the output
+
+
 fname = input("Input your First Name : ")
 lname = input("Input your Last Name : ")
 print ("Hello  " + fname + " " + lname)
