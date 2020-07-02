66f4b513e4eb29f88be2ff50e125798eaa495464
Jacob Wiltshire <jacobjameswiltshire@gmail.com>
2020-07-02 14:54:28 +0100

    wrote some code

---
+++ Sources/Challenges/62.py
@@ -0,0 +1,7 @@
+import turtle
+
+for i in range(1, 360):
+    turtle.forward(1)
+    turtle.left(1)
+
+turtle.exitonclick()
---
+++ Sources/Challenges/65.py
@@ -0,0 +1,46 @@
+import turtle
+
+turtle.left(90)
+turtle.forward(150)
+turtle.penup()
+
+#number 1 finish
+
+turtle.right(90)
+turtle.forward(50)
+turtle.pendown()
+
+#finish repositioning
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(75)
+turtle.right(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(75)
+turtle.left(90)
+turtle.forward(100)
+turtle.penup()
+
+#number 2 finsih
+
+turtle.forward(50)
+turtle.pendown()
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(75)
+turtle.left(90)
+turtle.forward(75)
+turtle.left(180)
+turtle.penup()
+turtle.forward(75)
+turtle.pendown()
+turtle.left(90)
+turtle.forward(75)
+turtle.left(90)
+turtle.forward(100)
+
+#number 3 finish
+
+turtle.exitonclick()
