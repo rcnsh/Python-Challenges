7756695c8aefeb4cbc9e5b6ccd4a7e06b20d65bf
Jacob Wiltshire <49075095+RCNOverwatcher@users.noreply.github.com>
2021-05-18 12:13:55 +0100

    Update 047.py

--- Sources/Challenges/047.py
+++ Sources/Challenges/047.py
@@ -1,8 +1,7 @@
 num1 = int(input("Pick a number"))
 num2 = int(input("Pick another number"))
 
-again = "y"
-while again = "y":
+while True:
     ask = input("Do you want to add another number?")
     if ask = "y":
         num = int(input(""))
