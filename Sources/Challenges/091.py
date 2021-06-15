73101e7e74ff0c48e1c1c54ce7e8a46d9205fbd2
Jacob Wiltshire <17WiltsJac@rhsstudents.co.uk>
2021-06-15 13:55:46 +0100

    py

--- Sources/Challenges/091.py
+++ Sources/Challenges/091.py
@@ -1,4 +1,5 @@
 import random
+from array import *
 
 num1=random.randint(1,100)
 num2=random.randint(1,100)
@@ -8,9 +9,16 @@ num5 = num1
 
 print("Enter one of these numbers:")
 
-mylist = [[num1], [num2], [num3], [num4]]
-random.shuffle(mylist)
+myarray = array('i',(num1,num2,num3,num4,num5))
+random.shuffle(myarray)
 
-for x in mylist:
-    print(x[0])
+for x in myarray:
+    print(x)
+
+asking=int(input(""))
+print("")
+print(myarray.count(asking))
+
+print("^^^ - This was how many times the number appeared in the array")
+
 
