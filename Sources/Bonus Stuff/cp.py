d7c92fdcf859dde8f57e8906c38f3b24366d3d95
RCNOverwatcher <jacobjameswiltshire@gmail.com>
2021-09-10 14:09:01 +0100

    cp

---
+++ Sources/Bonus Stuff/cp.py
@@ -0,0 +1,16 @@
+forename = input("Enter forename: ")
+surname = input("Enter surname: ")
+userp1 = forename + surname
+userp1 = userp1.lower()
+userp2 = "69420"
+userp2 = userp2.lower()
+userp3 = "TheGreat"
+userp3 = userp3.lower()
+userp4 = "_GD"
+userp4 = userp4.lower()
+
+print("Suggested username:", userp1 + userp2)
+print("Suggested username:", userp1 + userp3)
+print("Suggested username:", userp1 + userp4)
+
+username = input("Enter a chosen username: ")
